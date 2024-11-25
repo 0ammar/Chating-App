d450d34f07c59489960ca5370daa220e0914c7ea
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2024-11-25 08:59:25 +0300

    Add project files.

---
+++ Chating-App/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ChatingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatingApp.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UserController : ControllerBase
+	{
+		private readonly DataContext _context;
+		public UserController(DataContext context)
+		{
+			_context = context;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetAllUsers()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ Chating-App/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatingApp.Data
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<AppUser> Users { get; set; }
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
